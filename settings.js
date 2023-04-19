7502625626817530186a2e33774939fd9647e340
Hulgolli <108987513+Hulgolli@users.noreply.github.com>
2023-04-20 03:43:30 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ global.APIs = {
 //buy apikey premium 0887435047326
 // Free apikey (silahkan login terus ganti Your Key dgn apikey lu)
 global.APIKeys = {
-   'https://api.zeeoneofc.my.id': 'Your Key', // 👉 login https://api.zeeoneofc.my.id to get apikey
+   'https://api.zeeoneofc.my.id': 'Izhu4NVKrzYzqbD', // 👉 login https://api.zeeoneofc.my.id to get apikey
 }
 
 //language 
@@ -19,9 +19,9 @@ global.language = indonesia //change indonesia to english if you don't understan
 
 global.BOT_TOKEN = "6122822065:AAF_1sDArRvhMXSUz_NPMQ6uU-0-Cx2CvY8" //create bot here https://t.me/BotFather and get the bot token
 global.BOT_NAME = "ྂ✈ᵇᵒᵗ𝒈𝒊𝒓𝒍✔-Mdོ" //your bot name
-global.OWNER_NAME = "ᴹᴿ᭄ ྂᴋᴀɴɢ ᴋᴏᴍᴘᴏʀ🍓⃝﷽ོ ×፝֟͜×" //your name
+global.OWNER_NAME = "hallo_monyet" //your name
 global.OWNER_NUMBER = "16143244922" //your telegram number
 global.OWNER = ["https://t.me/hallo_monyet", "https://t.me/zeeone_ofc"] // pastikan username sudah sesuai agar fitur khusus owner bisa di pakai
-global.THUMBNAIL = "./image/ce5e26b35b5bcfd25406f4233cda9765.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
-global.DONASI = "./image/51dcc831533f269ce02c4b628240ba5e.jpg" // foto donasi di folder image
+global.THUMBNAIL = "./image/132dc00ab634d1717db1e77c77007922.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
+global.DONASI = "./image/1db3f54b71b17b098b1a138e390449ed.jpg" // foto donasi di folder image
 global.lang = language //don't change
